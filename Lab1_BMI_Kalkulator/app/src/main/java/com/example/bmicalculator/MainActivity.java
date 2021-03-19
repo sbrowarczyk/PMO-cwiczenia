2d644a624e17147ff36b8e5b257912cee3e45787
Sebastian Browarczyk <sbrowarczyk@wp.pl>
2021-03-19 14:04:21 +0100

    add project structure and layout

---
+++ Lab1_BMI_Kalkulator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //Wzór na BMI = masa ciała/wzrost
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
